a786285dff7fb45bb694a44e125a691dcc786778
Bartosz Nowotny <mapman17@gmail.com>
2023-01-19 01:31:25 +0100

    Parameterize app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import sys
+
 from coloring import get_orbits
 from rendering import render_basic
 
-for i, orbit in enumerate(sorted(get_orbits(2)), start=1):
+
+args = sys.argv[1:]
+assert len(args) == 1
+number_of_colors = int(args[0])
+assert 4 >= number_of_colors > 0
+
+for i, orbit in enumerate(sorted(get_orbits(number_of_colors)), start=1):
     render_basic(orbit, str(i))
